b0dfa9dbbeda896723d0540cec29cb8011cd428c
Saiful B. Khan <saifulbkhan@gmail.com>
2022-11-11 00:08:46 +0530

    Implement write functions for numeric types

--- internal/storage/recordalt.go
+++ internal/storage/recordalt.go
@@ -1,6 +1,9 @@
 package storage
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"math"
+)
 
 const (
 	INTEGER ElementType = 'i'
@@ -41,6 +44,10 @@ func (r *Record) offset(position uint16) uint16 {
 	return binary.LittleEndian.Uint16((*r)[4+2*position : 6+2*position])
 }
 
+func (r *Record) setOffset(position uint16, offset uint16) {
+	binary.LittleEndian.PutUint16((*r)[4+2*position:6+2*position], offset)
+}
+
 // NewRecord takes in the number of elements that will be stored in a record and returns a record
 // initialized with the appropriate length, header length and offsets for element positions. All
 // offsets are initialized to 0, meaning that the values for those element positions are null by
@@ -57,4 +64,56 @@ func NewRecord(numElements uint16) *Record {
 // Length returns the length of the record in bytes.
 func (r *Record) Length() uint16 {
 	return binary.LittleEndian.Uint16((*r)[0:2])
+}
+
+// SetUint32 saves the given uint32 value at the given element position in the record.
+func (r *Record) SetUint32(position uint16, value uint32) {
+	offset := r.offset(position)
+	if offset == 0 {
+		offset = r.Length()
+		r.setLength(offset + 4)
+		r.setOffset(position, offset)
+	}
+	*r = append(*r, byte(value), byte(value>>8), byte(value>>16), byte(value>>24))
+}
+
+// SetUint64 saves the given uint64 value at the given element position in the record.
+func (r *Record) SetUint64(position uint16, value uint64) {
+	offset := r.offset(position)
+	if offset == 0 {
+		offset = r.Length()
+		r.setLength(offset + 8)
+		r.setOffset(position, offset)
+	}
+	*r = append(
+		*r,
+		byte(value),
+		byte(value>>8),
+		byte(value>>16),
+		byte(value>>24),
+		byte(value>>32),
+		byte(value>>40),
+		byte(value>>48),
+		byte(value>>56),
+	)
+}
+
+// SetInt32 saves the given int32 value at the given element position in the record.
+func (r *Record) SetInt32(position uint16, value int32) {
+	r.SetUint32(position, uint32(value))
+}
+
+// SetInt64 saves the given int64 value at the given element position in the record.
+func (r *Record) SetInt64(position uint16, value int64) {
+	r.SetUint64(position, uint64(value))
+}
+
+// SetFloat32 saves the given float32 value at the given element position in the record.
+func (r *Record) SetFloat32(position uint16, value float32) {
+	r.SetUint32(position, math.Float32bits(value))
+}
+
+// SetFloat64 saves the given float64 value at the given element position in the record.
+func (r *Record) SetFloat64(position uint16, value float64) {
+	r.SetUint64(position, math.Float64bits(value))
 }
--- internal/storage/recordalt_test.go
+++ internal/storage/recordalt_test.go
@@ -49,3 +49,225 @@ func TestRecord_Length(t *testing.T) {
 		},
 	)
 }
+
+func TestRecord_SetUint32(t *testing.T) {
+	t.Run(
+		"check two elements", func(t *testing.T) {
+			r := NewRecord(2)
+			r.SetUint32(0, 10)
+			r.SetUint32(1, 20)
+
+			checkRecordLength(t, r, 16)
+			checkRecordBytes(t, r, 4, []byte{8, 0, 12, 0})
+			checkRecordBytes(t, r, 8, []byte{10, 0, 0, 0})
+			checkRecordBytes(t, r, 12, []byte{20, 0, 0, 0})
+		},
+	)
+
+	t.Run(
+		"check smallest uint32", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetUint32(0, 0)
+
+			checkRecordLength(t, r, 10)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{0, 0, 0, 0})
+		},
+	)
+
+	t.Run(
+		"check largest uint32", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetUint32(0, 4294967295)
+
+			checkRecordLength(t, r, 10)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 255, 255})
+		},
+	)
+}
+
+func TestRecord_SetUint64(t *testing.T) {
+	t.Run(
+		"check two elements", func(t *testing.T) {
+			r := NewRecord(2)
+			r.SetUint64(0, 10)
+			r.SetUint64(1, 20)
+
+			checkRecordLength(t, r, 24)
+			checkRecordBytes(t, r, 4, []byte{8, 0, 16, 0})
+			checkRecordBytes(t, r, 8, []byte{10, 0, 0, 0, 0, 0, 0, 0})
+			checkRecordBytes(t, r, 16, []byte{20, 0, 0, 0, 0, 0, 0, 0})
+		},
+	)
+
+	t.Run(
+		"check smallest uint64", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetUint64(0, 0)
+
+			checkRecordLength(t, r, 14)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{0, 0, 0, 0, 0, 0, 0, 0})
+		},
+	)
+
+	t.Run(
+		"check largest uint64", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetUint64(0, 18446744073709551615)
+
+			checkRecordLength(t, r, 14)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 255, 255, 255, 255, 255, 255})
+		},
+	)
+}
+
+func TestRecord_SetInt32(t *testing.T) {
+	t.Run(
+		"check two elements", func(t *testing.T) {
+			r := NewRecord(2)
+			r.SetInt32(0, 10)
+			r.SetInt32(1, -20)
+
+			checkRecordLength(t, r, 16)
+			checkRecordBytes(t, r, 4, []byte{8, 0, 12, 0})
+			checkRecordBytes(t, r, 8, []byte{10, 0, 0, 0})
+			checkRecordBytes(t, r, 12, []byte{236, 255, 255, 255})
+		},
+	)
+
+	t.Run(
+		"check smallest int32", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetInt32(0, -2147483648)
+
+			checkRecordLength(t, r, 10)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{0, 0, 0, 128})
+		},
+	)
+
+	t.Run(
+		"check largest int32", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetInt32(0, 2147483647)
+
+			checkRecordLength(t, r, 10)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 255, 127})
+		},
+	)
+}
+
+func TestRecord_SetInt64(t *testing.T) {
+	t.Run(
+		"check two elements", func(t *testing.T) {
+			r := NewRecord(2)
+			r.SetInt64(0, 10)
+			r.SetInt64(1, -20)
+
+			checkRecordLength(t, r, 24)
+			checkRecordBytes(t, r, 4, []byte{8, 0, 16, 0})
+			checkRecordBytes(t, r, 8, []byte{10, 0, 0, 0, 0, 0, 0, 0})
+			checkRecordBytes(t, r, 16, []byte{236, 255, 255, 255, 255, 255, 255, 255})
+		},
+	)
+
+	t.Run(
+		"check smallest int64", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetInt64(0, -9223372036854775808)
+
+			checkRecordLength(t, r, 14)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{0, 0, 0, 0, 0, 0, 0, 128})
+		},
+	)
+
+	t.Run(
+		"check largest int64", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetInt64(0, 9223372036854775807)
+
+			checkRecordLength(t, r, 14)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 255, 255, 255, 255, 255, 127})
+		},
+	)
+}
+
+func TestRecord_SetFloat32(t *testing.T) {
+	t.Run(
+		"check two elements", func(t *testing.T) {
+			r := NewRecord(2)
+			r.SetFloat32(0, 10.5)
+			r.SetFloat32(1, -20.5)
+
+			checkRecordLength(t, r, 16)
+			checkRecordBytes(t, r, 4, []byte{8, 0, 12, 0})
+			checkRecordBytes(t, r, 8, []byte{0, 0, 40, 65})
+			checkRecordBytes(t, r, 12, []byte{0, 0, 164, 193})
+		},
+	)
+
+	t.Run(
+		"check smallest float32", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetFloat32(0, -3.40282346638528859811704183484516925440e+38)
+
+			checkRecordLength(t, r, 10)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 127, 255})
+		},
+	)
+
+	t.Run(
+		"check largest float32", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetFloat32(0, 3.40282346638528859811704183484516925440e+38)
+
+			checkRecordLength(t, r, 10)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 127, 127})
+		},
+	)
+}
+
+func TestRecord_SetFloat64(t *testing.T) {
+	t.Run(
+		"check two elements", func(t *testing.T) {
+			r := NewRecord(2)
+			r.SetFloat64(0, 10.5)
+			r.SetFloat64(1, -20.5)
+
+			checkRecordLength(t, r, 24)
+			checkRecordBytes(t, r, 4, []byte{8, 0, 16, 0})
+			checkRecordBytes(t, r, 8, []byte{0, 0, 0, 0, 0, 0, 37, 64})
+			checkRecordBytes(t, r, 16, []byte{0, 0, 0, 0, 0, 128, 52, 192})
+		},
+	)
+
+	t.Run(
+		"check smallest float64", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetFloat64(0, -1.797693134862315708145274237317043567981e+308)
+
+			checkRecordLength(t, r, 14)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 255, 255, 255, 255, 239, 255})
+		},
+	)
+
+	t.Run(
+		"check largest float64", func(t *testing.T) {
+			r := NewRecord(1)
+			r.SetFloat64(0, 1.797693134862315708145274237317043567981e+308)
+
+			checkRecordLength(t, r, 14)
+			checkRecordBytes(t, r, 4, []byte{6, 0})
+			checkRecordBytes(t, r, 6, []byte{255, 255, 255, 255, 255, 255, 239, 127})
+		},
+	)
+}
